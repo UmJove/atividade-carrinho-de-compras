1a85fe4b43501853307dacfb4f8d0c186e480fa3
Luan Almada <lba.letras@gmail.com>
2025-04-04 11:20:27 -0300

    Primeira Versão

---
+++ js/app.js
@@ -0,0 +1,37 @@
+let carrinho = document.getElementById('lista-produtos');
+carrinho.innerHTML= '';
+let valorTotal = document.getElementById('valor-total');
+let total = 0;
+valorTotal.innerText = `R$ ${total}`;
+
+function adicionar() {
+    //recuperar nome, preço e quantidade do produto selecionado
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let precoProduto = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    
+    //calcular subtotal de cada produto
+    let subtotal = quantidade * precoProduto;
+
+    //adicionar produto ao carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${subtotal}</span>
+        </section>`
+
+    // atualizar total do produto
+    total = total + subtotal;
+    valorTotal.innerText = `R$ ${total}`;
+
+
+
+
+
+    // let valorTotal = document.getElementById('valor-total');
+    // valorTotal.innerHTML. = ;
+
+console.log('confirma4')
+
+}
+   
+   
